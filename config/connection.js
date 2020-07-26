cbd548701659b3810ad58a8b2a966be2f6c55d15
jdavis3333 <jdavis3333@gmail.com>
2020-07-25 18:59:06 -0700

    linking heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "baseball_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "baseball_db"
+  });  
+};
+
 
 connection.connect(function(err) {
   if (err) {
@@ -16,4 +22,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
